fdd7ebcd778b7f0f83468c8dda0724b5a13028ab
azhalkouski <azhalkouski@gmail.com>
2018-11-20 15:37:05 +0300

    format code in rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -3,20 +3,20 @@ import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
-	input: 'src/index.js',
-	output: {
-		file: 'dist/bundle.js',
+  input: 'src/index.js',
+  output: {
+    file: 'dist/bundle.js',
     format: 'cjs',
     name: 'ObservePropsDecorator',
-		sourcemap: true
-	},
-	plugins: [
-		resolve({
+    sourcemap: true
+  },
+  plugins: [
+    resolve({
       customResolveOptions: {
         moduleDirectory: 'node_modules'
       }
-    }), // tells Rollup how to find date-fns in node_modules
-    commonjs({ // converts date-fns to ES modules
+    }),
+    commonjs({
       include: 'node_modules/**'
     }),
     babel({
